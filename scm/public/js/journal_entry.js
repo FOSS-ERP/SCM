f865e6a25d202a43b9cf11b0e328b33cb0d80f3b
viralkansodiya <viral@fosserp.com>
2025-04-28 14:09:13 +0000

    set all accounting dimation in child table

--- scm/public/js/journal_entry.js
+++ scm/public/js/journal_entry.js
@@ -5,16 +5,19 @@ frappe.ui.form.on('Journal Entry', {
 
         // Fetch all Accounting Dimensions
         frappe.db.get_list('Accounting Dimension', {
-            fields: ['name'],
+            fields: ['name', "fieldname"],
         }).then(dimensions => {
             dimensions.forEach(dim => {
+                frm.fields_dict[dim.fieldname].df.onchange = ()=>{
+                    set_accounting_dimention_in_child(frm, dim.fieldname, frm.doc[dim.fieldname])
+                }
                 // Fetch Accounting Dimension Details for the current company and parent dimension
                 frappe.db.get_list('Accounting Dimension Detail', {
                     filters: { company: frm.doc.company, parent: dim.name },
                     fields: ['self_balancing_ledger'],
                     parent_doctype: "Accounting Dimension"
                 }).then(child_records => {
-
+                    console.log(child_records)
                     // Check if any record has 'self_balancing_ledger' set to 0
                     var is_editable = child_records.some(rec => rec.self_balancing_ledger === 0);
                     var fieldname = dim.name.toLowerCase(); // Convert dimension name to lowercase
@@ -24,7 +27,6 @@ frappe.ui.form.on('Journal Entry', {
                     
                     // var custom_fieldname = fieldname;
                     var custom_fieldname = fieldname;
-                    console.log(custom_fieldname);
                     
                     if(read_only) { 
                         frm.set_df_property(custom_fieldname, 'read_only', 0);
@@ -34,7 +36,6 @@ frappe.ui.form.on('Journal Entry', {
                         frm.set_df_property(custom_fieldname, 'reqd', 0);
                     }
                     
-                    console.log("Setting field:", fieldname, "Read-Only:", read_only);
 
                     // Ensure the field exists in the 'accounts' child table before applying the property
                     if (frm.fields_dict["accounts"]?.grid?.get_field(fieldname)) {
@@ -64,3 +65,8 @@ frappe.ui.form.on('Journal Entry', {
     }
 
 });
+function set_accounting_dimention_in_child(frm, fieldname, value){
+    frm.doc.accounts.forEach(r => {
+        frappe.model.set_value(r.doctype, r.name, fieldname, value)
+    })
+}

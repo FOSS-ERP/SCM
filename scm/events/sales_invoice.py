d27eb142f3649415373f58ddf1fe8ab6425f9f0f
Gautam <gautam@Basawarajs-MacBook-Air.local>
2026-01-16 11:54:20 +0530

    Upadted function disable_tax_included_prices_for_internal_transfer to execute only if checbox Fetch Valuation Rate for Internal Transaction is unchecked

--- scm/events/sales_invoice.py
+++ scm/events/sales_invoice.py
@@ -4,17 +4,22 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 
 class CustomSalesInvocie(SalesInvoice):
     def disable_tax_included_prices_for_internal_transfer(self):
-        if self.is_internal_transfer():
-            tax_updated = False
-            for tax in self.get("taxes"):
-                if tax.get("included_in_print_rate"):
-                    tax.included_in_print_rate = 1
-                    tax_updated = False
+        settings = frappe.get_cached_doc("Accounts Settings")
+        if settings.fetch_valuation_rate_for_internal_transaction:
+            super().disable_tax_included_prices_for_internal_transfer()
+        
+        else:
+            if self.is_internal_transfer():
+                tax_updated = False
+                for tax in self.get("taxes"):
+                    if tax.get("included_in_print_rate"):
+                        tax.included_in_print_rate = 1
+                        tax_updated = False
 
-            if tax_updated:
-                frappe.msgprint(
-                    _("Disabled tax included prices since this {} is an internal transfer").format(
-                    self.doctype
-                    ),
-                    alert=1,
-                )
+                if tax_updated:
+                    frappe.msgprint(
+                        _("Disabled tax included prices since this {} is an internal transfer").format(
+                        self.doctype
+                        ),
+                        alert=1,
+                    )
